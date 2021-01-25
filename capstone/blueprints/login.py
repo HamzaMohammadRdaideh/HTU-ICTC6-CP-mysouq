145daed8e2d40bec3138370cfddc10fe1ff24f25
Hamza Rdaideh <rdaidehhamza87@gmail.com>
2021-01-25 19:52:07 +0200

    create login form and login file in blueprints

---
+++ capstone/blueprints/login.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, render_template, request, redirect, session, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, validators, PasswordField
+from blog.forms import LoginForm
+from blog.models import User
+
+# define our blueprint
+login_bp = Blueprint('login', __name__)
+
+
+@login_bp.route('/login', methods=['POST', 'GET'])
+def login():
+
+    login_form = LoginForm()
+
+    if login_form.validate_on_submit():
+
+        # read values from the login wtform
+        username = login_form.username.data
+        password = login_form.password.data
+
+        user = User.objects(username=username).first()
+
+        # if user  != None:
+        # check if credentials are valid
+        if user and user.authenticate(username, password) :
+            # store the user ID in the session
+            session['uid'] = str(user.id)
+            session['username'] = user.username
+            session['email'] = user.email
+            session['role'] = user.role
+
+        return redirect("/profile")
+
+    # render the login template
+    return render_template('login/login.html', form=login_form)
+
+
+@login_bp.route('/logout')
+def logout():
+    # pop 'uid' from session
+    session.clear()
+
+    # redirect to index
+    return redirect("/")
